2e39b7076f937c6077b508cd5e965768eb8ec390
Jordan Naei <jordannaei.engineer@gmail.com>
2021-01-29 15:34:16 -0500

    added the enviorment node env variable

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ mongoose.connect(
 var app = express();
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.use(express.static("client"));
+// app.use(express.static("client"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
